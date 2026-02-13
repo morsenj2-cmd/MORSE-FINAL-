6a687569470440d97f0a71ee9be3284f4a03a7b6
Prayag <morsenj2@gmail.com>
2026-02-13 22:24:26 +0530

    add clerk oauth callback route

--- Morse2-main/client/src/App.tsx
+++ Morse2-main/client/src/App.tsx
@@ -1,5 +1,6 @@
 import { Switch, Route, Redirect } from "wouter";
 import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
+import { AuthenticateWithRedirectCallback } from "@clerk/nextjs";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -80,6 +81,7 @@ function Router() {
   return (
     <Switch>
       <Route path="/" component={Desktop} />
+      <Route path="/sso-callback" component={AuthenticateWithRedirectCallback} />
       <Route path="/about" component={AboutUs} />
       <Route path="/pricing" component={Pricing} />
       <Route path="/blog" component={Blog} />
