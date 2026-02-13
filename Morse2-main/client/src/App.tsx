7bbdbd2af5e93b9504668a53e95bf165dd029221
Prayag <morsenj2@gmail.com>
2026-02-13 22:31:30 +0530

    remove nextjs import final

--- Morse2-main/client/src/App.tsx
+++ Morse2-main/client/src/App.tsx
@@ -1,6 +1,6 @@
 import { Switch, Route, Redirect } from "wouter";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
-import { AuthenticateWithRedirectCallback } from "@clerk/nextjs";
+import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
